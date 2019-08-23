35c46ada30580fda016e517c2da45a2c03c9cc44
Lester Arteaga <lesterarte@gmail.com>
2019-08-23 12:27:15 -0600

    creating loggin

---
+++ ux_proyecto/src/components/Loggin.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class Loggin extends Component {
+  render() {
+    return (
+      <div className="bg-backgroud">
+        <div className="container">
+          <div className="card">
+            <div className="card-header">Inicia sesión</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- ux_proyecto/src/index.js
+++ ux_proyecto/src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'popper.js';
+import 'bootstrap/dist/css/bootstrap.css';
+window.jQuery = window.$ = $;
+require('datatables');
+
 
 ReactDOM.render(
   <App />,
