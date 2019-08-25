93403e7ea7563245d83c94729bd5a4ee7e221d80
Lester Arteaga <lesterarte@gmail.com>
2019-08-24 18:54:25 -0600

    fix some proplems Registro and loggin, logo modified

--- ux_proyecto/src/App.js
+++ ux_proyecto/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Principal from './components/Loggin/Loggin';
+import Registro from './components/Registro/Registro';
 
 class App extends Component {
   render() {
     return (
-           <Principal /> 
+           <Registro /> 
     );
   }
 }
