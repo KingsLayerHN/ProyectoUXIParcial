3415d94175ba5066d35a39afa9e396bb957f4c5a
Franklin-garcia <franklin__garcia@unitec.edu>
2019-08-31 02:45:07 -0600

    crear modal
    crear modal para crear clase y apuntarse a una clase

--- ux_proyecto/src/components/Principal/Principal.js
+++ ux_proyecto/src/components/Principal/Principal.js
@@ -3,7 +3,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import add from "./add.svg";
 
 export default class Principal extends Component {
-  render() {
+       
+render() {
     return (
       <nav className=" navbar  navbar-expand-lg navbar-light bg-light page-header">
         <div className="btn-group dropright">
@@ -21,10 +22,10 @@ export default class Principal extends Component {
             <FontAwesomeIcon icon={["fas", "plus"]} />
           </button>
           <div className="dropdown-menu">
-            <button className="dropdown-item" type="button">
+            <button className="dropdown-item" type="button" data-toggle="modal" data-target="#mostrar_apuntarse">
               Apuntarse en una clase
             </button>
-            <button className="dropdown-item" type="button">
+            <button className="dropdown-item" type="button"  data-toggle="modal" data-target="#mostrar_crear">
               Crear una clase
             </button>
           </div>
@@ -45,7 +46,62 @@ export default class Principal extends Component {
             </button>
           </div>
         </div>
+        <div className="modal" id="mostrar_apuntarse">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h4 className="modal-title">Apuntarse a una clase</h4> 
+                    </div>
+                
+                <div className="modal-body">
+                        <h6>Pídele el código de la clase a tu profesor e introdúcelo aquí.</h6>
+                       <form action="/action_page.php">
+                            <div className="form-group">
+                                <input className="form-control" placeholder="Codigo de la clase"></input>
+                            </div>
+                        </form>
+                </div>
+                
+                    <div className="modal-footer">
+                        <button type="button" className="btn">Unirse</button>
+                        <button type="button" className="btn" data-dismiss="modal">Cancelar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="modal fade" id="mostrar_crear">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    
+                <div className="modal-header">
+                        <h4 className="modal-title">Apuntarse a una clase</h4> 
+                    </div>
+                
+                <div className="modal-body">
+                        <h6>Crear una clase.</h6>
+                       <form action="/action_page.php">
+                                <input placeholder="Nombre de la clase (obligatorio)" className="form-control"></input>
+                            
+                                <input placeholder="Sección" className="form-control"></input>
+                             
+                                <input placeholder="Materia" className="form-control"></input>
+                            
+                                <input placeholder="Aula" className="form-control"></input>
+                            
+                        </form>
+                </div>
+                
+                    <div className="modal-footer">
+                        <button type="button" className="btn">Crear</button>
+                        <button type="button" className="btn" data-dismiss="modal">Cancelar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
       </nav>
+        
     );
   }
+    
+    
 }
