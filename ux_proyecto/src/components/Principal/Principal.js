584c9ef7f4f376e46df5ba0872e6acd947e91a47
Franklin-garcia <franklin__garcia@unitec.edu>
2019-09-01 21:22:39 -0600

    cmabiando los formularios de los modales
    mejorando los modales

--- ux_proyecto/src/components/Principal/Principal.js
+++ ux_proyecto/src/components/Principal/Principal.js
@@ -46,54 +46,102 @@ render() {
             </button>
           </div>
         </div>
+        
         <div className="modal" id="mostrar_apuntarse">
             <div className="modal-dialog">
                 <div className="modal-content">
                     <div className="modal-header">
                         <h4 className="modal-title">Apuntarse a una clase</h4> 
                     </div>
-                
                 <div className="modal-body">
-                        <h6>Pídele el código de la clase a tu profesor e introdúcelo aquí.</h6>
-                       <form action="/action_page.php">
-                            <div className="form-group">
-                                <input className="form-control" placeholder="Codigo de la clase"></input>
+                    <h6>Pídele el código de la clase a tu profesor e introdúcelo aquí.</h6>
+                </div>
+                <div className="modal-body">
+                       <form action="/action_page.php" className="was-validated">
+                            <div className="form-group form-check">
+                                <label className="form-check-label">
+                                    <div className="input-group-prepend">
+                                        <span1 className="input-group-text bg-info">
+                                            <h6 className="text-light">Código</h6>
+                                        </span1>
+                                    </div>
+                                    <input className="form-control" placeholder="Codigo de la clase" required></input>
+                                    <div className="valid-feedback">Entrada en proceso</div>
+                                    <div className="invalid-feedback">Por favor introduce el código</div>
+                                </label>
                             </div>
                         </form>
-                </div>
-                
+                </div>             
                     <div className="modal-footer">
-                        <button type="button" className="btn">Unirse</button>
-                        <button type="button" className="btn" data-dismiss="modal">Cancelar</button>
+                        <button type="button" className="btn bt-info">Unirse</button>
+                        <button type="button" className="btn btn-info" data-dismiss="modal">Cancelar</button>
                     </div>
                 </div>
             </div>
         </div>
+        
         <div className="modal fade" id="mostrar_crear">
             <div className="modal-dialog">
                 <div className="modal-content">
                     
-                <div className="modal-header">
-                        <h4 className="modal-title">Apuntarse a una clase</h4> 
-                    </div>
-                
+                <div className="modal-header text-center">
+                        <h1 className="text-info lead text-center">Crear una clase</h1> 
+                </div>
                 <div className="modal-body">
-                        <h6>Crear una clase.</h6>
-                       <form action="/action_page.php">
-                                <input placeholder="Nombre de la clase (obligatorio)" className="form-control"></input>
-                            
-                                <input placeholder="Sección" className="form-control"></input>
-                             
-                                <input placeholder="Materia" className="form-control"></input>
-                            
-                                <input placeholder="Aula" className="form-control"></input>
-                            
+                       <form action="/action_page.php" className="was-validated">
+                            <div className="form-group form-check">
+                                <label className="form-check-label">
+                                    <div className="input-group-prepend">
+                                    </div>
+                                   <input placeholder="Nombre de la clase (obligatorio)" className="form-control" required></input>
+                                    <div className="valid-feedback">Entrada en proceso</div>
+                                    <div className="invalid-feedback">Por favor introduce el nombre de la clase</div>
+                                </label>
+                            </div>        
                         </form>
                 </div>
-                
+                <div className="modal-body">
+                       <form action="/action_page.php" className="was-validated">
+                            <div className="form-group form-check">
+                                <label className="form-check-label">
+                                    <div className="input-group-prepend">
+                                    </div>
+                                   <input placeholder="Sección" className="form-control" required></input>
+                                    <div className="valid-feedback">Entrada en proceso</div>
+                                    <div className="invalid-feedback">Por favor introduce la sección</div>
+                                </label>
+                            </div>        
+                        </form>
+                </div>
+                <div className="modal-body">
+                       <form action="/action_page.php" className="was-validated">
+                            <div className="form-group form-check">
+                                <label className="form-check-label">
+                                    <div className="input-group-prepend">
+                                    </div>
+                                   <input placeholder="Materia" className="form-control" required></input>
+                                    <div className="valid-feedback">Entrada en proceso</div>
+                                    <div className="invalid-feedback">Por favor introduce la materia</div>
+                                </label>
+                            </div>        
+                        </form>
+                </div>
+                <div className="modal-body">
+                       <form action="/action_page.php" className="was-validated">
+                            <div className="form-group form-check">
+                                <label className="form-check-label">
+                                    <div className="input-group-prepend">
+                                    </div>
+                                   <input placeholder="Aula" className="form-control" required></input>
+                                    <div className="valid-feedback">Entrada en proceso</div>
+                                    <div className="invalid-feedback">Por favor introduce el aula</div>
+                                </label>
+                            </div>        
+                        </form>
+                </div>
                     <div className="modal-footer">
-                        <button type="button" className="btn">Crear</button>
-                        <button type="button" className="btn" data-dismiss="modal">Cancelar</button>
+                        <button type="button" className="btn btn-info">Crear</button>
+                        <button type="button" className="btn btn-info" data-dismiss="modal">Cancelar</button>
                     </div>
                 </div>
             </div>
