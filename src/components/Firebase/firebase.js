57b63e9698abec65294ee0c0b342450cdae6b2d6
lester Arteaga <lesterarte@gmail.com>
2019-09-05 22:28:49 -0600

    add a firebase to the project

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,9 @@
+const config = {
+    apiKey: "AIzaSyCy6KEMNQGPVMSH7Lgk72LHwwejwORHOmI",
+    authDomain: "aprende-f9370.firebaseapp.com",
+    databaseURL: "https://aprende-f9370.firebaseio.com",
+    projectId: "aprende-f9370",
+    storageBucket: "",
+    messagingSenderId: "882396673594",
+    appId: "1:882396673594:web:d2421648c7a89a14"
+}
