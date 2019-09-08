b55f6d767b2d086206161ae2b46d60e8b75c4fa7
Franklin-garcia <franklin__garcia@unitec.edu>
2019-09-07 23:28:55 -0600

    creacion de task
    creacion en el blackbord de un modal para el task

--- src/components/Blackboard/Blackboard.js
+++ src/components/Blackboard/Blackboard.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import BlackboardHeader from "./BlackboardHeader/BlackboardHeader";
 import BlackboardNav from "./BlackboardNav/BlackboardNav";
+import BlackboardTask from "./BlackboardTask/BlackboardTask";
 
 export default class Blackboard extends Component {
   render() {
     return (
       <div>
         <BlackboardNav />
-        <BlackboardHeader />
+        <BlackboardTask />
+        
       </div>
     );
   }
